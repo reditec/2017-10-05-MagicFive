b021deeaae0f166121bca458f4e90fb6242c1922
reditec <reditec@outlook.com>
2017-10-06 16:23:31 +0200

    10/06 update
    Works now
    
    Known bugs:
    - Program crashes when entering non numeric values.

--- src/Ratespiel.java
+++ src/Ratespiel.java
@@ -1,37 +1,45 @@
+import java.util.Scanner;
+
 /**
  * Magic Five
  * Ratespiel.java
  * Purpose: Guess that 5 is the magic number.
  *
+ * String t, tipp: save user input
+ *
  * @author Felix Bartling
- * @version 1.0 10/05/17
+ * @version 1.01 10/06/17
  * @since 10/05/2017
  */
 
 public class Ratespiel {
 
-    public Ratespiel()
-    {
+    private Ratespiel() {
         System.out.println("Willkommen");
 
-        //TO-DO: declare function "input" --> will be fixed during next lecture
         String t = input("Erraten Sie die Zahl: ");
 
+        //crashes if t is not numeric
         int tipp = Integer.parseInt(t);
 
         if(tipp == 5) {
-            System.out.println("Gewonnen");
+            System.out.println("Gewonnen!");
         }
-        else
-        {
-            System.out.println("Verloren");
+        else {
+            System.out.println("Verloren!");
         }
 
-        System.out.println("Das Spiel ist aus");
+        System.out.println("Game over");
 
     }
 
-    /* public static void main(String[] args) {
+    private String input(String ausgabe) {
+        System.out.println(ausgabe);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
 
-    }*/
+    public static void main(String[] args) {
+        Ratespiel meinRatespiel = new Ratespiel();
+    }
 }
